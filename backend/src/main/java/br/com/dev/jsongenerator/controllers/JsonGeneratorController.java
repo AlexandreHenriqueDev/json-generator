356e3b484271c43036a01f00313e6414be52a496
Alexandre Henrique <alexandre.henrique.dev@gmail.com>
2021-05-20 10:36:21 -0300

    Correção no pom.xml referente ao problemas na compatibilidade das libs com o Java 11
    Anotação no serviço para descrever no swagger

--- backend/src/main/java/br/com/dev/jsongenerator/controllers/JsonGeneratorController.java
+++ backend/src/main/java/br/com/dev/jsongenerator/controllers/JsonGeneratorController.java
@@ -2,6 +2,7 @@ package br.com.dev.jsongenerator.controllers;
 
 import br.com.dev.jsongenerator.dto.ObjectReaderDto;
 import br.com.dev.jsongenerator.services.JsonGeneratorService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,10 @@ public class JsonGeneratorController {
     private JsonGeneratorService service;
 
     @PostMapping(value = "/getObject")
+    @ApiOperation(httpMethod = "POST",
+            value = "Retorna um novo Json com valores randomicos ou estáticos de acordo com o objeto passado",
+            response = ResponseEntity.class,
+            nickname="getObject")
     public ResponseEntity<ResponseBody> getObject(@RequestBody List<ObjectReaderDto> listObjectReader) {
         return service.processGenericObject(listObjectReader);
     }
