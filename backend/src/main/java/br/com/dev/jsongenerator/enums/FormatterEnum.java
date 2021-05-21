b69fd6e91d9bc20ffe24f730d6dcdabbb3e62612
Alexandre Henrique <alexandre.henrique.dev@gmail.com>
2021-05-21 15:05:33 -0300

    Alterando description de CNPJ

--- backend/src/main/java/br/com/dev/jsongenerator/enums/FormatterEnum.java
+++ backend/src/main/java/br/com/dev/jsongenerator/enums/FormatterEnum.java
@@ -3,7 +3,7 @@ package br.com.dev.jsongenerator.enums;
 public enum FormatterEnum {
 
     CPF("CPF"),
-    CNPJ("CPF"),
+    CNPJ("CNPJ"),
     FUTURE_DATE("FUTURE_DATE"),
     PAST_DATE("PAST_DATE"),
     RANDOM_DATE("RANDOM_DATE");
