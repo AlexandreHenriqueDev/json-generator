0d88eee306d06eb3478b6b3bda89e5d5bc157948
Alexandre Henrique <alexandre.henrique.dev@gmail.com>
2021-05-21 15:00:16 -0300

    Adicionando final as contantes

--- backend/src/main/java/br/com/dev/jsongenerator/constants/DateConstants.java
+++ backend/src/main/java/br/com/dev/jsongenerator/constants/DateConstants.java
@@ -2,11 +2,13 @@ package br.com.dev.jsongenerator.constants;
 
 public class DateConstants {
 
-    public static Integer YEARS_PAST_INTERVAL = 2;
-    public static Integer YEARS_FUTURE_INTERVAL = 10;
+    public static final Integer YEARS_PAST_INTERVAL = 2;
+    public static final Integer YEARS_FUTURE_INTERVAL = 10;
 
-    public static Integer MONTHS_FUTURE_INTERVAL = 12;
+    public static final Integer MONTHS_FUTURE_INTERVAL = 12;
 
-    public static Integer DAYS_FUTURE_INTERVAL = 30;
+    public static final Integer DAYS_FUTURE_INTERVAL = 30;
 
+    private DateConstants() {
+    }
 }
